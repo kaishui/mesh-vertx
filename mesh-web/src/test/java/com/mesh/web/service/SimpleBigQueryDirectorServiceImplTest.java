6ff1ac15a2bd05b1e60bd211c146a232d26e378b
kaishui <yiqiesuifengf@163.com>
2023-05-08 18:35:34 +0800

    sql builder

--- mesh-web/src/test/java/com/mesh/web/service/SimpleBigQueryDirectorServiceImplTest.java
+++ mesh-web/src/test/java/com/mesh/web/service/SimpleBigQueryDirectorServiceImplTest.java
@@ -9,9 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class SimpleBigQueryDirectorServiceImplTest {
--- web-core/src/main/java/com/mesh/web/core/MainVerticle.java
+++ web-core/src/main/java/com/mesh/web/core/MainVerticle.java
@@ -2,6 +2,15 @@ package com.mesh.web.core;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.openapi.RouterBuilder;
+import io.vertx.ext.web.validation.RequestParameter;
+import io.vertx.ext.web.validation.RequestParameters;
+import io.vertx.ext.web.validation.ValidationHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,18 +19,53 @@ import org.springframework.stereotype.Component;
 public class MainVerticle extends AbstractVerticle {
 
   @Override
+
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+
+    Promise<Router> promise = Promise.promise();
+    RouterBuilder.create(vertx, "openapi.yaml")
+      .onSuccess(routerBuilder -> {
+        routerBuilder
+          .operation("listPets")
+          .handler(routingContext -> {
+            RequestParameters params =
+              routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+            RequestParameter body = params.body();
+            JsonObject jsonBody = body.getJsonObject();
+            log.info("successful to load openapi.yaml, {}", jsonBody);
+//            routingContext
+//              .response()
+//              .setStatusCode(200)
+//              .setStatusMessage(jsonBody.toString())
+//              .end();
+          })
+          .failureHandler(routingContext -> {
+            // Handle failure
+            log.error("failed", routingContext.failed());
+
+          });
+
+        Router router = routerBuilder.createRouter();
+
+        promise.complete(router);
+      })
+      .onFailure(err -> {
+        log.error("start failed", err);
+      });
+
+    promise.future().onSuccess(router -> {
+      router.route("/*").handler(StaticHandler.create());
+
+      HttpServer server = vertx.createHttpServer();
+      server.requestHandler(router).listen(8888, http -> {
+        if (http.succeeded()) {
+          startPromise.complete();
+          System.out.println("HTTP server started on port 8888");
+        } else {
+          startPromise.fail(http.cause());
+        }
+      });
     });
+
   }
 }
