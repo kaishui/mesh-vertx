ad65d3f62e56b84e75668ed3fc11c4a3f1672547
kaishui <yiqiesuifengf@163.com>
2023-05-11 18:19:55 +0800

    sql builder

--- mesh-web/src/main/java/com/mesh/web/controller/JsonToSql.java
+++ mesh-web/src/main/java/com/mesh/web/controller/JsonToSql.java
@@ -279,9 +279,8 @@ public class JsonToSql {
               sb.append(parseSwitch((JsonObject) v)).append(" as ").append(key).append(", ");
             }
           }
-        } else if (val instanceof String) {
+        } else if (val instanceof String str) {
           // 如果值是一个字符串，表示有别名操作符，如$as, $alias等，用as关键字表示，并给结果起别名为值
-          String str = (String) val;
           if (isAliasOperator(str)) {
             sb.append(parseAliasOperator(str, key)).append(", ");
           }
@@ -306,14 +305,12 @@ public class JsonToSql {
     for (String key : object.fieldNames()) {
       Object val = object.getValue(key);
       if (key.equals("branches")) {        // 如果键是branches，表示有多个分支条件，根据值的类型进行处理
-        if (val instanceof JsonArray) {
-          JsonArray array = (JsonArray) val;
+        if (val instanceof JsonArray array) {
           // 遍历数组中的每个元素
           for (int i = 0; i < array.size(); i++) {
             Object elem = array.getValue(i);
             // 如果元素是一个对象，解析对象中的属性，并返回when和then子句
-            if (elem instanceof JsonObject) {
-              JsonObject obj = (JsonObject) elem;
+            if (elem instanceof JsonObject obj) {
               Object caseVal = obj.getValue("case");
               Object thenVal = obj.getValue("then");
               if (caseVal != null && thenVal != null) {
@@ -339,8 +336,7 @@ public class JsonToSql {
   // 解析$group操作符的值，并返回group by子句
   public static String parseGroup(Object value) {
     // 如果值是一个对象，解析对象中的属性，并返回group by子句
-    if (value instanceof JsonObject) {
-      JsonObject object = (JsonObject) value;
+    if (value instanceof JsonObject object) {
       StringBuilder sb = new StringBuilder();
       sb.append("group by ");
       // 遍历对象中的每个键值对
@@ -351,9 +347,8 @@ public class JsonToSql {
           if (val instanceof String) {
             // 如果值是一个字符串，直接返回字段名
             sb.append(val).append(", ");
-          } else if (val instanceof JsonArray) {
+          } else if (val instanceof JsonArray array) {
             // 如果值是一个数组，用逗号连接每个元素，并返回字段名列表
-            JsonArray array = (JsonArray) val;
             for (int i = 0; i < array.size(); i++) {
               sb.append(array.getString(i)).append(", ");
             }
@@ -372,17 +367,15 @@ public class JsonToSql {
   // 解析$sort操作符的值，并返回order by子句
   public static String parseSort(Object value) {
     // 如果值是一个对象，解析对象中的属性，并返回order by子句
-    if (value instanceof JsonObject) {
-      JsonObject object = (JsonObject) value;
+    if (value instanceof JsonObject object) {
       StringBuilder sb = new StringBuilder();
       sb.append("order by ");
       // 遍历对象中的每个键值对
       for (String key : object.fieldNames()) {
         Object val = object.getValue(key);
         // 如果键是一个字段名，根据值的类型进行处理
-        if (val instanceof Number) {
+        if (val instanceof Number num) {
           // 如果值是一个数字，表示排序顺序，1表示升序，-1表示降序
-          Number num = (Number) val;
           if (num.intValue() == 1) {
             sb.append(key).append(" asc, ");
           } else if (num.intValue() == -1) {
@@ -407,8 +400,7 @@ public class JsonToSql {
   // 解析$limit操作符的值，并返回limit子句
   public static String parseLimit(Object value) {
     // 如果值是一个数字，直接返回limit子句
-    if (value instanceof Number) {
-      Number num = (Number) value;
+    if (value instanceof Number num) {
       return "limit " + num.intValue();
     }
     // 如果值是其他类型，返回空字符串
@@ -418,8 +410,7 @@ public class JsonToSql {
   // 解析$skip操作符的值，并返回offset子句
   public static String parseSkip(Object value) {
     // 如果值是一个数字，直接返回offset子句
-    if (value instanceof Number) {
-      Number num = (Number) value;
+    if (value instanceof Number num) {
       return "offset " + num.intValue();
     }
     // 如果值是其他类型，返回空字符串
@@ -433,20 +424,20 @@ public class JsonToSql {
 
   // 解析逻辑操作符，并返回相应的sql关键字
   public static String parseLogicalOperator(String key, Object value) {
-    switch (key) {
-      case "$or":
+    return switch (key) {
+      case "$or" ->
         // 如果是$or操作符，用or连接每个元素，并用括号包围
-        return parseArrayOperator("or", value);
-      case "$and":
+        parseArrayOperator("or", value);
+      case "$and" ->
         // 如果是$and操作符，用and连接每个元素，并用括号包围
-        return parseArrayOperator("and", value);
-      case "$not":
+        parseArrayOperator("and", value);
+      case "$not" ->
         // 如果是$not操作符，用not关键字表示，并用括号包围
-        return "not (" + parseJson((JsonObject) value) + ")";
-      default:
+        "not (" + parseJson((JsonObject) value) + ")";
+      default ->
         // 如果是其他操作符，返回空字符串
-        return "";
-    }
+        "";
+    };
   }
 
   // 解析数组类型的操作符，并返回相应的sql关键字
@@ -476,29 +467,29 @@ public class JsonToSql {
 
   // 解析比较操作符，并返回相应的sql符号
   public static String parseComparisonOperator(String key, Object value) {
-    switch (key) {
-      case "$gt":
+    return switch (key) {
+      case "$gt" ->
         // 如果是$gt操作符，用>号表示
-        return " > " + value;
-      case "$lt":
+        " > " + value;
+      case "$lt" ->
         // 如果是$lt操作符，用<号表示
-        return " < " + value;
-      case "$eq":
+        " < " + value;
+      case "$eq" ->
         // 如果是$eq操作符，用=号表示
-        return " = " + value;
-      case "$gte":
+        " = " + value;
+      case "$gte" ->
         // 如果是$gte操作符，用>=号表示
-        return " >= " + value;
-      case "$lte":
+        " >= " + value;
+      case "$lte" ->
         // 如果是$lte操作符，用<=号表示
-        return " <= " + value;
-      case "$ne":
+        " <= " + value;
+      case "$ne" ->
         // 如果是$ne操作符，用<>号表示
-        return " <> " + value;
-      default:
+        " <> " + value;
+      default ->
         // 如果是其他操作符，返回空字符串
-        return "";
-    }
+        "";
+    };
   }
 
   // 判断是否是聚合操作符
@@ -537,8 +528,7 @@ public class JsonToSql {
       return func + "(" + value + ")";
     }
     // 如果值是一个数组，用逗号连接每个元素，并返回函数名和字段名列表
-    if (value instanceof JsonArray) {
-      JsonArray array = (JsonArray) value;
+    if (value instanceof JsonArray array) {
       StringBuilder sb = new StringBuilder();
       sb.append(func).append("(");
       for (int i = 0; i < array.size(); i++) {
@@ -561,24 +551,23 @@ public class JsonToSql {
 
   // 解析别名操作符，并返回相应的sql关键字
   public static String parseAliasOperator(String key, Object value) {
-    switch (key) {
-      case "$as":
+    return switch (key) {
+      case "$as" ->
         // 如果是$as操作符，用as关键字表示，并返回字段名和别名
-        return value + " as " + key;
-      case "$alias":
+        value + " as " + key;
+      case "$alias" ->
         // 如果是$alias操作符，用as关键字表示，并返回字段名和别名
-        return value + " as " + key;
-      default:
+        value + " as " + key;
+      default ->
         // 如果是其他操作符，返回空字符串
-        return "";
-    }
+        "";
+    };
   }
 
   // 解析$match操作符的值，并返回where子句
   public static String parseMatch(Object value) {
     // 如果值是一个对象，解析对象中的属性，并返回where子句
-    if (value instanceof JsonObject) {
-      JsonObject object = (JsonObject) value;
+    if (value instanceof JsonObject object) {
       StringBuilder sb = new StringBuilder();
       sb.append("where ");
       // 遍历对象中的每个键值对
@@ -589,18 +578,16 @@ public class JsonToSql {
           sb.append("(").append(parseLogicalOperator(key, val)).append(")");
         } else {
           // 如果键是一个字段名，根据值的类型进行处理
-          if (val instanceof JsonObject) {
+          if (val instanceof JsonObject obj) {
             // 如果值是一个对象，表示有比较操作符，如$gt, $lt, $eq等，用相应的sql符号表示
-            JsonObject obj = (JsonObject) val;
             for (String k : obj.fieldNames()) {
               Object v = obj.getValue(k);
               if (isComparisonOperator(k)) {
                 sb.append(key).append(parseComparisonOperator(k, v));
               }
             }
-          } else if (val instanceof JsonArray) {
+          } else if (val instanceof JsonArray array) {
             // 如果值是一个数组，表示有in或者not in操作符，用相应的sql关键字表示，并用括号包围
-            JsonArray array = (JsonArray) val;
             if (array.size() > 0) {
               Object elem = array.getValue(0);
               if (elem instanceof String && isNegationOperator((String) elem)) {
@@ -628,14 +615,13 @@ public class JsonToSql {
   // 解析数组类型的操作符，并返回相应的sql关键字
   public static String parseArrayOperator(String op, Object value) {
     // 如果值是一个数组，用指定的操作符连接每个元素，并用括号包围
-    if (value instanceof JsonArray) {
-      JsonArray array = (JsonArray) value;
+    if (value instanceof JsonArray array) {
       StringBuilder sb = new StringBuilder();
       sb.append("(");
       for (int i = 0; i < array.size(); i++) {
         Object elem = array.getValue(i);
         if (elem instanceof JsonObject) {
-          sb.append(parseJson((JsonObject) elem));
+          sb.append(parseCondition((JsonObject) elem));
         } else if (elem instanceof JsonArray) {
           sb.append(parseArrayOperator(op, elem));
         } else {
@@ -650,6 +636,96 @@ public class JsonToSql {
     }
     // 如果值是其他类型，返回空字符串
     return "";
+  }
+
+  // 定义一个辅助方法，用于解析json对象表示的条件，并转化为sql语句中的条件
+  public static String parseCondition(JsonObject condition) {
+    // 初始化一个字符串缓冲区，用于拼接条件语句
+    StringBuilder sb = new StringBuilder();
+    // 遍历条件对象的键集合
+    for (String key : condition.fieldNames()) {
+      // 获取键对应的值，它可能是一个整数，一个字符串数组，一个json对象或者一个对象数组，表示不同的比较或者逻辑操作
+      Object value = condition.getValue(key);
+      // 根据不同的键，拼接相应的条件语句
+      switch (key) {
+        case "$or" -> {
+          // 拼接或操作，如(score > 70 and score < 90 ) or views >= 1000
+          sb.append("(");
+          JsonArray orArray = (JsonArray) value;
+          for (int i = 0; i < orArray.size(); i++) {
+            JsonObject orObj = orArray.getJsonObject(i);
+            sb.append(parseCondition(orObj)).append(" or ");
+          }
+          sb.delete(sb.length() - 4, sb.length());
+          sb.append(")");
+        }
+        case "$and" -> {
+          // 拼接与操作，如avg(scores) >= 80 and avg(scores) < 90
+          sb.append("(");
+          JsonArray andArray = (JsonArray) value;
+          for (int i = 0; i < andArray.size(); i++) {
+            JsonObject andObj = andArray.getJsonObject(i);
+            sb.append(parseCondition(andObj)).append(" and ");
+          }
+          sb.delete(sb.length() - 5, sb.length());
+          sb.append(")");
+        }
+        case "$gt" -> {
+          // 拼接大于操作，如score > 70
+          int gtValue = (Integer) value;
+          sb.append(key).append(" > ").append(gtValue);
+        }
+        case "$lt" -> {
+          // 拼接小于操作，如score < 90
+          int ltValue = (Integer) value;
+          sb.append(key).append(" < ").append(ltValue);
+        }
+        case "$gte" -> {
+          // 拼接大于等于操作，如views >= 1000
+          int gteValue = (Integer) value;
+          sb.append(key).append(" >= ").append(gteValue);
+        }
+        case "$avg" -> {
+          // 拼接平均操作，如avg(scores)
+          String avgColumn = (String) value;
+          sb.append("avg(").append(avgColumn).append(")");
+        }
+        default -> {
+          // 如果键不是以上的特殊操作符，表示是一个列名，如score或views
+          // 获取值对应的json对象，表示要进行的比较操作，如{ $gt: 70, $lt: 90 }
+          JsonObject compareObj = (JsonObject) value;
+          // 遍历比较对象的键集合
+          for (String compareKey : compareObj.fieldNames()) {
+            // 获取键对应的值，它是一个整数，表示要比较的数值，如70或90
+            int compareValue = compareObj.getInteger(compareKey);
+            // 根据不同的比较键，拼接相应的条件语句
+            switch (compareKey) {
+              case "$gt" ->
+                // 拼接大于操作，如score > 70
+                sb.append(key).append(" > ").append(compareValue);
+              case "$lt" ->
+                // 拼接小于操作，如score < 90
+                sb.append(key).append(" < ").append(compareValue);
+              case "$gte" ->
+                // 拼接大于等于操作，如views >= 1000
+                sb.append(key).append(" >= ").append(compareValue);
+              default -> {
+              }
+            }
+            // 拼接逻辑与操作符，因为同一个列名可能有多个比较条件，如score > 70 and score < 90
+            sb.append(" and ");
+          }
+          // 删除最后一个多余的逻辑与操作符和空格
+          sb.delete(sb.length() - 5, sb.length());
+        }
+      }
+      // 拼接逻辑与操作符，因为不同的列名可能有不同的比较条件，如(score > 70 and score < 90 ) and views >= 1000
+      sb.append(" and ");
+    }
+    // 删除最后一个多余的逻辑与操作符和空格
+    sb.delete(sb.length() - 5, sb.length());
+    // 返回条件语句字符串
+    return sb.toString();
   }
 
   // 解析数组，并返回用逗号和括号包围的字符串
